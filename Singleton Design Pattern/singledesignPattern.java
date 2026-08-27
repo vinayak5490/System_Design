dca0b1eb21a84738e8813d2ea27fc52b3f9c4be2
vinayak5490 <149044261+vinayak5490@users.noreply.github.com>
2026-08-28 01:44:12 +0530

    singleton design pattern completed

---
+++ Singleton Design Pattern/singledesignPattern.java
@@ -0,0 +1,29 @@
+/**
+ * singledesignPattern
+ */
+public class singledesignPattern {
+    static class Singleton{
+        private static Singleton instance;
+        private Singleton(){
+
+        }
+        public static Singleton getInstance(){
+            if(instance == null){// avoids synchronization when object already exsist
+                //not created yet
+                synchronized(Singleton.class){
+                    if(instance == null){
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+    
+}
